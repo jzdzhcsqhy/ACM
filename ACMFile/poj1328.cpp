29c2b681d39c8e8a565e9aec40f39ffaedb038c2
jzdzhcsqhy <466921396@163.com>
2015-04-07 14:22:15 +0800

    2015年4月7日 14:22:06

---
+++ ACMFile/poj1328.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+typedef struct  
+{
+	double s;
+	double e;
+} Rs;
+
+int main ( int argc, char** argv )
+{
+	int n, d;
+	int iCnt =0;
+	while( scanf("%d %d",&n, &d ) )
+	{
+		iCnt ++;
+		cout << "Case " << iCnt << ": ";
+
+		if( 0 == n && 0 == d )
+		{
+			return 0;
+		}
+		int x, y;
+		while( scanf("%d %d",&x, &y ) )
+		{
+			float 
+		}
+
+	}
+
+	return 0;
+}
---
+++ ACMFile/poj2965.cpp
@@ -0,0 +1,65 @@
+#include "iostream"
+#include <queue>
+#include <map>
+using namespace std;
+
+typedef struct 
+{	
+	int x;
+	int y;
+}Step;
+
+bool mark[4][4];
+
+int main( int argc, char** argv )
+{
+
+	char ch;
+	memset(mark, 0, sizeof(mark) );
+	
+	int i,j,k;
+	queue<Step> q;
+	
+	for( i=0; i<4; i++ )
+	{
+		for( j=0; j<4; j++ )
+		{
+			ch = getchar();
+			if('+' == ch  )
+			{
+				mark[i][j] = !mark[i][j];
+				for( int k=0; k<4; k++ )
+				{
+					mark[i][k] = !mark[i][k];  
+					mark[k][j] = ! mark[k][j]; 
+				}
+			}
+		}
+		getchar();
+	}
+
+	for( i=0; i<4; i++ )
+	{
+		for( j=0; j<4; j++ )
+		{
+			if( mark[i][j])
+			{
+				Step tmp;
+				tmp.x = i+1;
+				tmp.y = j+1;
+				q.push(tmp);
+			}
+		}
+	}
+	cout << q.size() << endl;
+	while( !q.empty() )
+	{
+		Step tmp = q.front();
+		q.pop();
+		cout << tmp.x << " " << tmp.y << endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
